620585ce214ddd2637141139eb8b379f903024b4
AlexDeveloper12 <angrycoder95@gmail.com>
2023-02-26 19:37:12 +0000

    Adding subtitle property

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { AiFillBook } from 'react-icons/ai';
 
 function Book({ item }) {
+    console.log(item.volumeInfo);
 
     return (
         <div className="col-md-4">
@@ -14,17 +16,22 @@ function Book({ item }) {
                     <h5 className="card-title">
                         {item.volumeInfo.title}
                     </h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{item.volumeInfo.subtitle} </h6>
                     <p className="card-text">
-
                         {item.searchInfo !== undefined ?
 
                             <span>{item.searchInfo.textSnippet}</span>
 
                             : null
                         }
-
+                        <span>
                         {item.volumeInfo.publishedDate}
+                        </span>
+
+                        
                     </p>
+                    <a href="#" className="card-link"> <AiFillBook size={25} color="black" /> Add to bookshelf </a>
+                    <a href="#" className="card-link">Show more</a>
                 </div>
             </div>
         </div>
