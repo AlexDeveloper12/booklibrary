9361ef09e78cae91ef403daf2f67657e88fce26c
AlexDeveloper12 <angrycoder95@gmail.com>
2023-03-06 21:08:03 +0000

    Adding material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App'
 import Bookshelf from './components/Bookshelf';
-import './index.css'
+import './index.css';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
